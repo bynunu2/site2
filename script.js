0ac537e06ff7e6f8f997a14073be2bd773acb965
Núbia Eugênio <nunu.eugenios@gmail.com>
2024-09-10 10:42:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Função para rolar suavemente para as seções quando os links são clicados 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+}); 
+
